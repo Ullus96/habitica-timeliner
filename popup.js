12829570deada0c8dc39cf05576b9a253f772eb3
Ullus <nikita96piter@gmail.com>
2024-12-24 03:52:27 +0300

    Save API Token to chrome.storage.local

--- popup.js
+++ popup.js
@@ -1,2 +1,13 @@
-const timelineContainer = document.getElementById('timeline-container');
-console.log(timelineContainer);
+let habiticaApiKey = '';
+const timelineElement = document.getElementById('timeline-container');
+
+document.getElementById('api-form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	const apiKey = document.getElementById('api-key').value;
+	chrome.storage.local.set({ habiticaApiKey: apiKey });
+});
+
+chrome.storage.local.get(['habiticaApiKey'], (result) => {
+	habiticaApiKey = result.habiticaApiKey;
+	timelineElement.innerText = habiticaApiKey;
+});
